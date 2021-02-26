4b36db2e8c1258a74cf3cac0f0d52545670ea557
Nick <njarosz1@gmail.com>
2021-02-26 16:38:50 -0700

    added CSV path

--- pushcsv.sh
+++ pushcsv.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 
-cd /home/pi/Documents/
+cd /home/pi/Documents/CSV/
 cp $(ls -1t | head -1) /home/pi/tj_counter
 git -C /home/pi/tj_counter/ add .
 git -C /home/pi/tj_counter/ commit -m "csv"
