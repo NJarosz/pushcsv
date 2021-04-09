086ef64ca9dd15b0b94bac715cb3a85c5bead8df
NJarosz <63024003+NJarosz@users.noreply.github.com>
2021-04-08 21:29:47 -0600

    Update pushcsv.sh

--- pushcsv.sh
+++ pushcsv.sh
@@ -4,6 +4,7 @@
 
 cd /home/pi/Documents/CSV
 cp $(ls -1t | head -1) /home/pi/pushcsv
+git -C /home/pi/pushcsv/ checkout pi02
 git -C /home/pi/pushcsv/ add .
 git -C /home/pi/pushcsv/ commit -m "csv"
 git -C /home/pi/pushcsv/ push
